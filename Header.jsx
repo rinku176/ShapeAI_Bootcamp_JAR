0783c6f9613beaed79878ac5510b5b9fb1efb457
rinku176 <74528780+rinku176@users.noreply.github.com>
2021-06-28 02:46:17 +0400

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>LION KING</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Hakuna Matata!</h1>
+      <p>We only live once boiz. So lets have a lot of fun! </p>
+    </div>
+    
+  );
+}
+
+export default Info;
